b55e153b90be5aef65bc02c7be85b02e5f2bab99
Justin G. Mitchell <jgmitchell@gmail.com>
2023-12-02 10:42:54 -0700

    Solved

--- aoc02.py
+++ aoc02.py
@@ -1,15 +1,11 @@
 # Advent of Code - Day 02
 import aoc_utils
 
+# load input
 input_data = aoc_utils.load_input("day02/input")
 
-max_cubes = {
-    "red": 12,
-    "green": 13,
-    "blue": 14,
-}
 
-
+# Get the details of each game
 def get_game_data(d):
     my_data = {}
     for game in d:
@@ -22,20 +18,70 @@ def get_game_data(d):
         my_data[int(game_number)] = game_reveal
     return my_data
 
-def is_game_valid(game_data):
-    for game in game_data:
-        reveal = game_data[game]
-        print(reveal)
-        
-        '''c = c.strip("\n")
-        cube_details = c.split(",")
-        for color in cube_details:
-            p = color.strip()
-            p = color.split()
-            if p[1] == "red" and int(p[0]) < 12:
-                pass
-        print(game_number, cube_details)'''
-
-
 
 game_data = get_game_data(input_data)
+
+
+# Test each game for valid results
+def remove_invalid(game_data):
+    check = []
+    for game in game_data:
+        maybe_valid = True
+        reveal = game_data[game]
+        for r in reveal:
+            r = r.split(",")
+            for n in r:
+                n = n.split()
+                if int(n[0]) > 14:
+                    maybe_valid = False
+                if int(n[0]) > 12 and n[1] == "red":
+                    maybe_valid = False
+                if int(n[0]) > 13 and n[1] == "green":
+                    maybe_valid = False
+                
+        if maybe_valid:
+            check.append(game)
+    return check
+
+
+possible = remove_invalid(game_data)
+
+
+# Check possible trult valid results
+def check_possible(possible):
+    total = 0
+    for p in possible:
+        total = total + int(p)
+    print(total)
+
+
+check_possible(possible)
+
+# Part 2
+# Find the minimum cubes for each reveal
+
+def get_cubes(game_data):
+    total_sum_of_powers = 0
+    for game in game_data:
+        red = 0
+        blue = 0
+        green = 0
+        reveals = game_data[game]
+        for hand in reveals:
+            ind = hand.split(",")
+            for cube_set in ind:
+                cube_count = cube_set.split()
+                count = int(cube_count[0])
+                if "red" in cube_count:
+                    if count > red:
+                        red = count
+                if "blue" in cube_count:
+                    if count > blue:
+                        blue = count
+                if "green" in cube_count:
+                    if count > green:
+                        green = count
+        total_sum_of_powers = total_sum_of_powers + (red * blue * green)
+    print(total_sum_of_powers)
+
+get_cubes(game_data)
