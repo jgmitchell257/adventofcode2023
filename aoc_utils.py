54c1fa43861a2c1b8fee8751a2c19403c9afb2aa
Justin G. Mitchell <jgmitchell@gmail.com>
2023-12-05 06:51:38 -0700

    Strip newlines from input data

--- aoc_utils.py
+++ aoc_utils.py
@@ -3,4 +3,8 @@
 def load_input(x):
     input_file = open(x,"r")
     input_data = input_file.readlines()
-    return input_data
+    output_data = []
+    for line in input_data:
+        line = line.strip("\n")
+        output_data.append(line)
+    return output_data
